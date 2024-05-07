a4df03d76bb93f65babfd6bcccacfcd8fe8ba038
HilmanBintang22 <149690667+HilmanBintang22@users.noreply.github.com>
2024-05-07 16:46:41 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,14 @@
+print("start")
+
+bilangan_pertama = float(input("Masukkan bilangan pertama: "))
+bilangan_kedua = float(input("Masukkan bilangan kedua: "))
+
+hasil_penjumlahan = bilangan_pertama + bilangan_kedua
+hasil_pengurangan = bilangan_pertama - bilangan_kedua
+hasil_modulus = bilangan_pertama % bilangan_kedua
+
+print(f"Hasil penjumlahan: {hasil_penjumlahan}")
+print(f"Hasil pengurangan: {hasil_pengurangan}")
+print(f"Hasil modulus: {hasil_modulus}")
+
+print("end")
